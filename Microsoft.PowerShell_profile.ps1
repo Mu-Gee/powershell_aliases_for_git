2a4a0516256d69aa10496c21bc6bfa87ea2a9d54
MuGee <61751306+Mu-Gee@users.noreply.github.com>
2023-11-10 18:22:09 +0300

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,121 @@
+New-Alias -Name init -Value gitinit
+
+function gitinit() {
+	$arg1 = "init"
+	Start-Process git -ArgumentList $arg1 -Wait -NoNewWindow
+}
+
+New-Alias -Name fetch -Value gitfetch
+
+function gitfetch() {
+    $arg1 = "fetch"
+    Start-Process git -ArgumentList $arg1 -Wait -NoNewWindow
+}
+
+New-Alias -Name merge -Value gitmerge
+
+function gitmerge(){
+    $arg1 = "merge"
+    Start-Process git -ArgumentList $arg1 -Wait -NoNewWindow
+}
+
+New-Alias -Name push -Value gitpush
+
+function gitpush(){
+    $arg1 = "push"
+    Start-Process git -ArgumentList $arg1 -Wait -NoNewWindow
+}
+
+New-Alias -Name pull -Value gitpull
+
+function gitpull(){
+    $arg1 = "pull"
+    Start-Process git -ArgumentList $arg1 -Wait -NoNewWindow
+}
+
+New-Alias -Name status -Value gitstatus
+
+function gitstatus(){
+    $arg1 = "status"
+    Start-Process git -ArgumentList $arg1 -Wait -NoNewWindow
+}
+
+New-Alias -Name log -Value gitlog
+function gitlog{
+    $arg1 = "log"
+    Start-Process git -ArgumentList $arg1 -Wait -NoNewWindow
+}
+
+###...THESE ALIASES BELOW TAKE ARGUMNETS LIKE <branch-name> <url> AND SO ON...###
+New-Alias -Name branch -Value gitBranch
+
+function gitBranch([string]$branchName) {
+	$arg1 = "branch"
+	$allArgs = @($arg1, $branchName)
+	Start-Process git -ArgumentList $allArgs -Wait -NoNewWindow
+}
+
+New-Alias -Name clone -Value gitclone
+
+function gitclone([string]$url) {
+	$arg1 = "clone"
+    $allArgs = @($arg1, $url)
+	Start-Process git -ArgumentList $allArgs -Wait -NoNewWindow
+}
+
+New-Alias -Name add -Value gitadd
+
+function gitadd([string]$file){
+    $arg1 = "add"
+    $allArgs = @($arg1, $file)
+    Start-Process git -ArgumentList $allArgs -Wait -NoNewWindow
+}
+
+New-Alias -Name commit -Value gitcommit
+
+function gitcommit([string]$message){
+    $arg1 = "commit"
+    $arg2 = "-m"
+    $allArgs = @($arg1, $arg2, $message)
+    Start-Process git -ArgumentList $allArgs -Wait -NoNewWindow
+}
+
+New-Alias -Name checkout -Value gitcheckout
+
+function gitcheckout([string]$branchName){
+    $arg1 = "checkout"
+    $allArgs = @($arg1, $branchName)
+    Start-Process git -ArgumentList $allArgs -Wait -NoNewWindow
+}
+
+New-Alias -Name gdiff -Value gitdiff
+
+function gitdiff([string]$branchName1, [string]$branchName2){
+    $arg1 = "diff"
+    $allArgs = @($arg1, $branchName1, $branchName2 )
+    Start-Process git -ArgumentList $allArgs -Wait -NoNewWindow
+}
+
+New-Alias -Name show -Value gitshow
+
+function gitshow([string]$commit){
+    $arg1 = "show"
+    $allArgs = @($arg1, $commit)
+    Start-Process git -ArgumentList $allArgs -Wait -NoNewWindow
+}
+
+New-Alias -Name branchd -Value gitbranchd
+
+function gitbranchd([string]$branchName){
+    $arg1 = "branch"
+    $arg2 = "-d"
+    $allArgs = @($arg1, $arg2, $branchName)
+    Start-Process git -ArgumentList $allArgs -Wait -NoNewWindow
+}
+
+New-Alias -Name reset -Value gitreset
+function gitreset([string]$commit){
+    $arg1 = "reset"
+    $allArgs = @($arg1, $commit)
+    Start-Process git -ArgumentList $allArgs -Wait -NoNewWindow
+}
